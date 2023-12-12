bae97ad531ec140759faf4c67da3c0786ea19429
mohammadrashid <mohammadrashid6732@gamil.com>
2023-12-12 18:11:33 +0330

    install react-query and use it in to the project

--- front/query/src/main.tsx
+++ front/query/src/main.tsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+/** @format */
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const client =  new QueryClient({});
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+	<React.StrictMode>
+		<QueryClientProvider client={client}>
+			<App />
+		</QueryClientProvider>
+	</React.StrictMode>
+);
