f7cedcff5603f6e8f7281114d5a7f2b966b91451
mohammadrashid <mohammadrashid6732@gamil.com>
2023-12-16 19:37:23 +0330

    add Query devTools and add it in to the main.jsx components and use it

--- front/query/src/main.tsx
+++ front/query/src/main.tsx
@@ -1,17 +1,19 @@
 /** @format */
 
-import React from 'react';
+// import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
-const client =  new QueryClient({});
+const client = new QueryClient({});
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-	<React.StrictMode>
-		<QueryClientProvider client={client}>
-			<App />
-		</QueryClientProvider>
-	</React.StrictMode>
+	// <React.StrictMode>
+	<QueryClientProvider client={client}>
+		<App />
+		<ReactQueryDevtools />
+	</QueryClientProvider>
+	// </React.StrictMode>
 );
